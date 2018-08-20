59b83cc6fc5c030483d30ee025083dc8d2d2247a
lycfight <1063866263@qq.com>
2018-08-20 22:26:02 +0800

    更新 densenet.py

--- nets/densenet.py
+++ nets/densenet.py
@@ -64,7 +64,41 @@ def densenet(images, num_classes=1001, is_training=False,
             ##########################
             # Put your code here.
             ##########################
+            net = slim.conv2d(images, 2*growth, [7, 7], stride=2, scope='Conv1')
+            end_points['Conv1']=net
+            net = slim.max_pool2d(net, [3, 3], stride=2, scope='pool1')
+            end_points['pool1'] = net
+            
+            net = block(net, 6, growth, scope='block1')
+            end_points['block1'] = net
+            net = bn_act_conv_drp(net,  reduce_dim(net), [1, 1], scope = 'transition1' )
+            net = slim.avg_pool2d(net, [2, 2], stride=2, scope='pool2')
+            end_points['pool2'] = net
 
+            net = block(net, 6, growth, scope='block2')
+            end_points['block2'] = net
+            net = bn_act_conv_drp(net,  reduce_dim(net), [1, 1], scope = 'transition2' )
+            net = slim.avg_pool2d(net, [2, 2], stride=2, scope='trans2_avgPool')
+            end_points['trans2_avgPool'] = net
+
+            net = block(net, 6, growth, scope='block3')
+            end_points['block3'] = net
+            net = bn_act_conv_drp(net,  reduce_dim(net), [1, 1], scope = 'transition3' )
+            net = slim.avg_pool2d(net, [2, 2], stride=2, scope='trans3_avgPool')
+            end_points['trans3_avgPool'] = net
+
+            net = block(net, 6, growth, scope='block4')
+            end_points['block4'] = net
+            net = tf.reduce_mean(net, [1, 2], keep_dims=True)
+            end_points['global_average_pooling'] = net
+            net = slim.flatten(net)
+            end_points['Flatten'] = net
+
+            logits = slim.fully_connected(net, num_classes, scope='logits')
+            end_points['logits'] = logits
+            #softmax
+            net =tf.nn.softmax(net, name='Predictions')
+            end_points['Predictions'] = net
     return logits, end_points
 
 
